2ba4b152bd931e4e9b483a55da1c75b029fb3b96
mason-e <engelmason@gmail.com>
2025-08-26 21:02:33 -0600

    Remove now-unused methods

--- helpers/job_base.py
+++ helpers/job_base.py
@@ -47,54 +47,6 @@ class JobBase(Parent):
         if self.click_next(self.NEXT_XPATH):
             self.get_results_by_card()
 
-    def get_results_mixed(self):
-        # method for getting all key info directly, with assumption it will be in order
-        self.titles += self.get_text_results(self.TITLE_XPATH)
-        self.companies += self.get_text_results(self.COMPANY_XPATH)
-        self.locations += self.get_text_results(self.LOCATION_XPATH)
-        self.links += self.get_link_results(self.TITLE_XPATH)
-        if self.click_next(self.NEXT_XPATH):
-            self.get_results_mixed()
-        else:
-            self.correct_mixed_results()
-
-    def correct_mixed_results(self):
-        # used when one data point can't be found in a mixed result - fill in with "unknown"
-        count = max((len(self.titles), len(self.companies), len(self.locations), len(self.links)))
-        if len(self.titles) == 0:
-            self.fill_list_with_unknown(self.titles, count)
-        if len(self.companies) == 0:
-            self.fill_list_with_unknown(self.companies, count)
-        if len(self.locations) == 0:
-            self.fill_list_with_unknown(self.locations, count)
-        if len(self.links) == 0:
-            self.fill_list_with_unknown(self.links, count)
-
-    def fill_list_with_unknown(self, list, length):
-        for i in range(length):
-            list.append("unknown")
-
-    def make_entries(self):
-        # use if the data is mixed into separate lists
-        if self.validate_results():
-            for (title, company, location, link) in zip(self.titles, self.companies, self.locations, self.links):
-                self.entries.append(JobEntry(title, company, location, link))
-
-    def validate_results(self):
-        # final check that all data is present - this could still be an issue if 
-        # the mismatched list size was not zero
-        validation = len(self.titles) == len(self.companies) and \
-                     len(self.companies) == len(self.locations)
-
-        if not validation:
-            print("Something wrong with results! Element sizes did not match.")
-            print("Titles found: " + len(self.titles).__str__())
-            print("Companies found: " + len(self.companies).__str__())
-            print("Locations found: " + len(self.locations).__str__())
-            print("Links found: " + len(self.links).__str__())
-
-        return validation
-
     def write_to_db(self):
         database = sql_helper.SqlDB()
         queries = query_helper.QueryHelpers()
